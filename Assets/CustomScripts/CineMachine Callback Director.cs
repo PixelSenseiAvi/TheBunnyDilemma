69b0e8cf53b233dfeb6e3b7f86658c97bbe9aed6
Avi Garg <a7vi1912@gmail.com>
2019-04-10 03:23:09 +0100

    check 5
    Lighting, post

---
+++ Assets/CustomScripts/CineMachine Callback Director.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class CineMachineCallbackDirector : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    void OnEnable()
+    {
+        director.paused += OnPlayableDirectorPaused;
+    }
+
+    void OnPlayableDirectorPaused(PlayableDirector aDirector)
+    {
+        if (director == aDirector)
+            Debug.Log("PlayableDirector named " + aDirector.name + " is now paused.");
+    }
+
+    void OnDisable()
+    {
+        director.paused -= OnPlayableDirectorPaused;
+    }
+}
+
---
+++ Assets/OrbitalCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalCamera : MonoBehaviour
+{
+    public float speed = 5f;
+    public float distance = 3f;
+
+    public Transform target;
+
+    private Vector2 input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        input += new Vector2(Input.GetAxis("Mouse X") * speed,
+            Input.GetAxis("Mouse Y") * speed);
+        Quaternion rotation = Quaternion.Euler(input.y, input.x, 0);
+
+        Vector3 position = target.position - (rotation * Vector3.forward * distance);
+        transform.localRotation = rotation;
+        transform.localPosition = position;
+    }
+}
