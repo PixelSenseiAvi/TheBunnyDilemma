d5c8783fea461fc57ae3525b64fd21464a2e939a
Avi Garg <a7vi1912@gmail.com>
2019-04-19 20:44:03 +0100

    Submission

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -10,8 +10,11 @@ public class Pause : MonoBehaviour
     public float playedTime;
     private int count;
 
+    public float slowdownFactor = 0.05f;
+    public float slowdownLength = 2f;
 
-   // Start is called before the first frame update
+
+    // Start is called before the first frame update
     void Start()
     {
         //pd = brute.GetComponent<PlayableDirector>();
@@ -25,6 +28,33 @@ public class Pause : MonoBehaviour
     {
         playedTime += Time.deltaTime;
         //playable.GetGraph().GetRootPlayable(0).GetTime(). 
+
+        //implement a better solution
+        if (playedTime >= 14f && playedTime <= 17f)
+        {
+            Time.timeScale = 0.005f;
+            //Time.fixedDeltaTime = Time.timeScale * .02f;
+        }
+        //tried to ease-out but implement a better solution
+        else if (playedTime > 17f && playedTime <= 18f)
+        {
+            if (Time.timeScale < 1f)
+            { Time.timeScale += 0.005f; }
+            if (playedTime > 17.4f && Time.timeScale != 1f)
+            {
+                Time.timeScale = 1f;
+            }
+        }
+        //same as above
+        //else if (playedTime >= 29.45f && playedTime <= 30.55f)
+        //{
+        //    Time.timeScale = 0.005f;
+        //} else if (playedTime > 30.55f && Time.timeScale != 1f)
+        //{
+        //    Time.timeScale = 1f;
+        //}
+
+
         if (playedTime >= 29.0f && playedTime <= 31.4f)
         {
             count++;
---
+++ Assets/SloMo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class SloMo : MonoBehaviour
+{
+    public float playedTime;
+
+    Vignette vignette = null;
+
+    private PostProcessVolume volume;
+    public GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playedTime = 0.0f;
+        volume = gameObject.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out vignette);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        playedTime += Time.deltaTime;
+        
+        if (playedTime >= 14f && playedTime <= 17f)
+        {
+            vignette.enabled.value = true;
+            vignette.intensity.value = 1f;
+            vignette.smoothness.value = 1f;
+            vignette.roundness.value = 1f;
+        }
+        else { vignette.enabled.value = false; }
+
+    }
+}
